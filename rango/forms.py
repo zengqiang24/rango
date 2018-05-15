8e303bb0191439036ef3b7a64d2a1d9577ef3b74
qzeng <qzeng@telenav.cn>
2018-05-15 18:12:09 +0800

    update  urls support dango 1.9

--- rango/forms.py
+++ rango/forms.py
@@ -5,21 +5,28 @@ from rango.models import Category, Page
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes=forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug=forms.IntegerField(widget=forms.HiddenInput(), required=False)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.IntegerField(widget=forms.HiddenInput(), required=False)
 
     # A inline class to provide additional information on the form
     class Meta:
-          model=Category
-          fields=('name',)
+        model = Category
+        fields = ('name',)
 
 
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="please enter the title of ")
+    url = forms.URLField(max_length=200, help_text="please enter the url of page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-class PageForm:
-    title=forms.CharField(max_length=128,help_text="please enter the title of ")
-    url= forms.URLField(max_length=200,help_text="please enter the url of page.")
-    views=forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        if url and not url.startWith('http://'):
+            url = "http://" + url
+            cleaned_data['url'] = url
+        return cleaned_data
 
     class Meta:
-        model=Page
-        exclude=('category',)
+        model = Page
+        exclude = ('category',)
