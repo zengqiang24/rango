16c286e8eb5921a78d2856b5becc64d076cc0f0b
qzeng <qzeng@telenav.cn>
2018-05-23 14:11:14 +0800

    add registration feature

--- rango/views.py
+++ rango/views.py
@@ -77,10 +77,30 @@ def add_page(request, category_name_slug):
 def register(request):
     registered=False
 
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
 
         if user_form.is_valid() and profile_form.is_valid():
-            user_form.save(commit=True)
-            
+          user=  user_form.save()
+          user.set_password=(user.password)
+          user.save()
+
+          profile=profile_form.save(commit=False)
+          profile.user=user
+
+          if  'picture' in request.FILES :
+              profile.picture=request.FILES['picture']
+
+              profile.save()
+              registered=True
+
+
+        else:
+            print(user_form.errors,profile_form.errors)
+
+    else:
+        user_form=UserForm()
+        profile_form=UserProfileForm()
+
+    return render(request,'rango/register.html',{'user_form':user_form,'profile_form':profile_form,'registered':registered})
