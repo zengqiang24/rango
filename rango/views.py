4fa445be0c6ab096f732202702b46122ae6e6bc0
qzeng <qzeng@telenav.cn>
2018-06-08 17:47:56 +0800

    add session

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -12,12 +14,42 @@ def index(request):
     context_dict = {'categories': category_list}
     category_views_list = Category.objects.order_by('-views')[:5]
     context_dict['category_views_list'] = category_views_list
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
     return render(request, 'rango/index.html', context_dict)
 
 
 @login_required
 def about(request):
     context_dict = {"about": "this is qiang blogs"}
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+
+    # remember to include the visit data
+    context_dict['visits']=count
     return render(request, 'rango/about.html', context_dict)
 
 
